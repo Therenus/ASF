699360e50a07b35c26c298d2c50098fe37e65b4b
Therenus <svoboan@gmail.com>
2023-09-22 21:49:37 +0200

    Frontend API call update

--- client/src/Apis/BookFinder.js
+++ client/src/Apis/BookFinder.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-// server
-//  baseURL: "https://academicsourcefinder.org/apiv1.0.0",
-//  local
-  baseURL: "http://localhost:3001/apiv1.0.0",
+  baseURL: window.location.href + "apiv1.0.0",
 });
